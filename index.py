2d623cf0d0bac6dccf59cf4254e51a025be25fc0
Nisha R S <nisha021rs@gmail.com>
2026-06-03 22:36:48 +0530

    Performing data cleaning as few datas are missing for age, embarked,fare.

--- index.py
+++ index.py
@@ -2,4 +2,10 @@ import pandas as pd
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 print(train.head()) #Loads the first rows to check if those r correct
-print(test.head())
+# print(test.head())
+print(train.info())
+print(train.describe())
+
+#Data Cleaning
+train["Age"] = train["Age"].fillna(train["Age"].median())
+test["Age"] = test["Age"].fillna(test["Age"])
