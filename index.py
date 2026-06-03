6cdcb5c564825e261e4e3d54555025388812397b
Nisha R S <nisha021rs@gmail.com>
2026-06-03 16:16:47 +0530

    Exporting the test.csv file to the index.py file

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ import pandas as pd
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 print(train.head()) #Loads the first rows to check if those r correct
-# print(test.head())
+print(test.head())
