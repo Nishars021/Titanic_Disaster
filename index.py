1d3036ab0fadbe613387bcd81c07eba8b0b8ee13
Nisha R S <nisha021rs@gmail.com>
2026-06-03 16:12:54 +0530

    Added all files to the folder.

--- index.py
+++ index.py
@@ -1,1 +1,5 @@
-import pandas as pandas
+import pandas as pd
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+print(train.head()) #Loads the first rows to check if those r correct
+print(test.head())
