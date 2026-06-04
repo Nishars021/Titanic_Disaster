881c670f307a105ef564db8e1e49e97648dbeae3
Nisha R S <nisha021rs@gmail.com>
2026-06-04 09:08:16 +0530

    Add display statement for both.

--- index.py
+++ index.py
@@ -17,13 +17,17 @@ test = pd.read_csv("test.csv")
 # print(train["Embarked"])
 
 #Filling missing values of Fare
-train["Fare"] = train["Fare"].fillna(train["Fare"].median())
-print(train["Fare"])
+# train["Fare"] = train["Fare"].fillna(train["Fare"].median())
+# print(train["Fare"])
 
 #Converting te Text into Numbers as the machines cannot understand Text :
 #(Sex)
 train["Sex"] = train["Sex"].map({"male":0,"female":1})
 test["Sex"] = test["Sex"].map({"male":0,"female":1})
+print(train["Sex"])
+print(test["Sex"])
 #(Embarked)
 train["Embarked"] = train["Embarked"].map({"S":0,"C":1,"Q":2})
 test["Embarked"] = test["Embarked"].map({"S":0,"C":1,"Q":2})
+print(train["Embarked"])
+print(test["Embarked"])
