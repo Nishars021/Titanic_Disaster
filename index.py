c6bfbcdae919cfa4dc920b25678fa756b5a80237
Nisha R S <nisha021rs@gmail.com>
2026-06-05 08:22:14 +0530

    added train & test dataset.

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import pandas as pd
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-print(train.head()) 
+print(train.head())
 #Loads the first rows to check if those r correct
 print(test.head())
 # print(train.info())
