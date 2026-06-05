7b0382cb693f6474fd95508f6dcab9b1b382c86a
Nisha R S <nisha021rs@gmail.com>
2026-06-05 09:07:39 +0530

    Performing task.

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 import pandas as pd
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-print(train.head())
+# print(train.head())
 #Loads the first rows to check if those r correct
-print(test.head())
+# print(test.head())
 # print(train.info())
 # print(train.describe())
 
