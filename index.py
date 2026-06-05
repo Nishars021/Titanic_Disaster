f28b523df60ecca380bb0ee02c8dcdc08ea1acca
Nisha R S <nisha021rs@gmail.com>
2026-06-05 11:33:14 +0530

    Completed with this.

--- index.py
+++ index.py
@@ -1,65 +1,65 @@
 import pandas as pd
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-# print(train.head())
+print(train.head())
 #Loads the first rows to check if those r correct
 # print(test.head())
 # print(train.info())
 # print(train.describe())
 
-# Data Cleaning
-# Filling missing values of Age
-train["Age"] = train["Age"].fillna(train["Age"].median())
-test["Age"] = test["Age"].fillna(test["Age"].median())
-# print(train["Age"])
+# # Data Cleaning
+# # Filling missing values of Age
+# train["Age"] = train["Age"].fillna(train["Age"].median())
+# test["Age"] = test["Age"].fillna(test["Age"].median())
+# # print(train["Age"])
 
-# Filling missing values of Embarked
-train["Embarked"] = train["Embarked"].fillna(train["Embarked"].mode()[0]) #Used 'mode' as it is a category"
-# print(train["Embarked"])
+# # Filling missing values of Embarked
+# train["Embarked"] = train["Embarked"].fillna(train["Embarked"].mode()[0]) #Used 'mode' as it is a category"
+# # print(train["Embarked"])
 
-# Filling missing values of Fare
-train["Fare"] = train["Fare"].fillna(train["Fare"].median())
-# print(train["Fare"])
+# # Filling missing values of Fare
+# train["Fare"] = train["Fare"].fillna(train["Fare"].median())
+# # print(train["Fare"])
 
-#Converting te Text into Numbers as the machines cannot understand Text :
-#(Sex)
-train["Sex"] = train["Sex"].map({"male":0,"female":1})
-test["Sex"] = test["Sex"].map({"male":0,"female":1})
-# print(train["Sex"])
-# print(test["Sex"])
-#(Embarked)
-train["Embarked"] = train["Embarked"].map({"S":0,"C":1,"Q":2})
-test["Embarked"] = test["Embarked"].map({"S":0,"C":1,"Q":2})
-# print(train["Embarked"])
-# print(test["Embarked"])
+# #Converting te Text into Numbers as the machines cannot understand Text :
+# #(Sex)
+# train["Sex"] = train["Sex"].map({"male":0,"female":1})
+# test["Sex"] = test["Sex"].map({"male":0,"female":1})
+# # print(train["Sex"])
+# # print(test["Sex"])
+# #(Embarked)
+# train["Embarked"] = train["Embarked"].map({"S":0,"C":1,"Q":2})
+# test["Embarked"] = test["Embarked"].map({"S":0,"C":1,"Q":2})
+# # print(train["Embarked"])
+# # print(test["Embarked"])
 
-#Selecting Features :
-features = ["Pclass","Sex","Age","Embarked","Fare","SibSp","Parch",]
+# #Selecting Features :
+# features = ["Pclass","Sex","Age","Embarked","Fare","SibSp","Parch",]
 
-#Prepare X & Y :
-X = train[features]
-y = train["Survived"]
-X_test = test[features]
+# #Prepare X & Y :
+# X = train[features]
+# y = train["Survived"]
+# X_test = test[features]
 
-#Train the model
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier (
-n_estimators = 100, #Generate 100 trees & understand the pattern
-random_state = 42 #Used to get same answer everytime & '42' just randomly used
-)
-model.fit(X,y) #Learn the pattern
+# #Train the model
+# from sklearn.ensemble import RandomForestClassifier
+# model = RandomForestClassifier (
+# n_estimators = 100, #Generate 100 trees & understand the pattern
+# random_state = 42 #Used to get same answer everytime & '42' just randomly used
+# )
+# model.fit(X,y) #Learn the pattern
 
 
-#Make Predictions
-predictions = model.predict(X_test)
+# #Make Predictions
+# predictions = model.predict(X_test)
 
-#Create a submission file
-submission = pd.DataFrame({
-    "PassengerID" : test["PassengerId"],
-    "Survived" : predictions
-})
+# #Create a submission file
+# submission = pd.DataFrame({
+#     "PassengerID" : test["PassengerId"],
+#     "Survived" : predictions
+# })
 
-#Save CSV
-submission.to_csv("submission.csv",index=False)
-print(submission.head())
-print("Submission file created successfully..!!!")
+# #Save CSV
+# submission.to_csv("submission.csv",index=False)
+# print(submission.head())
+# print("Submission file created successfully..!!!")
